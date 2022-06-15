98327ac77f6fb35169dc20ed0c4fe6c4bc143d22
vishnu244 <vishnunali244@gmail.com>
2022-06-15 20:23:27 +0530

    [Vishnu] Added multiple Employee Details And Determined Execution Time

---
+++ EmployeePayRoll_ADO.Net/OperationWIthThreads.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRoll_ADO.Net
+{
+    public class OperationWIthThreads
+    {
+
+        public List<Employee_details2> EmployeeDetails = new List<Employee_details2>();
+        public void addEmployeeToPayRoll(List<Employee_details2> employeeDetails)
+        {
+            employeeDetails.ForEach(employeeData =>
+            {
+                Console.WriteLine("Employee being added:" + employeeData.FirstName);
+                this.addEmployeePayRoll(employeeData);
+                Console.WriteLine("Employee Added :" + employeeData.FirstName);
+            });
+            //Console.WriteLine(this.EmployeeDetails.ToString());
+        }
+
+
+        public void addEmployeePayRoll(Employee_details2 emp)
+        {
+            EmployeeDetails.Add(emp);
+        }
+
+        public void addEmployeeToPayRollWithThread(List<Employee_details2> employeeDetails)
+        {
+            employeeDetails.ForEach((employeeData) =>
+            {
+                DateTime StartdateTime = DateTime.Now;
+
+                Console.WriteLine("Employee being added:" + employeeData.FirstName);
+                this.addEmployeePayRoll(employeeData);
+                Console.WriteLine("Employee Added :" + employeeData.FirstName);
+
+                DateTime StopDataTime = DateTime.Now;
+                Console.WriteLine("Duration without Thread: " + (StopDataTime - StartdateTime));
+
+            });
+        }
+    }
+}
